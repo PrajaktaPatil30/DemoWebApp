2934480222a6a6c34c0d72bc8453c3744cb4fde1
coder47kk <39201083+coder47kk@users.noreply.github.com>
2021-01-18 18:37:44 +0530

    created controller.

--- BookExchange/src/main/java/com/DemoWebApp/BookExchange/controller/BookExchangeApplicationController.java
+++ BookExchange/src/main/java/com/DemoWebApp/BookExchange/controller/BookExchangeApplicationController.java
@@ -3,9 +3,8 @@ package com.DemoWebApp.BookExchange.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 
 import com.DemoWebApp.BookExchange.model.User;
